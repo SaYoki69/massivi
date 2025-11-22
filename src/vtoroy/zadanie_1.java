33d7586c0938c3b5d3d0acf553b0e0c290ebd7bf
Student 3 <Student 3@student3>
2025-11-22 11:41:30 +0300

    добавление кода

--- src/vtoroy/zadanie_1.java
+++ src/vtoroy/zadanie_1.java
@@ -15,7 +15,37 @@ public class zadanie_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Заменить положительные элементы двумерного массива на 1, а\n" +
+"отрицательные на 0.");
+        int[][] array = new int[3][3];
+    for (int i = 0; i < array.length; i++){
+        for (int j = 0; j < array.length; j++){
+            array[i][j] = (int) (Math.random() * 20 - 10);
+            System.out.print(array[i][j] + "\t");
+        }
+        System.out.println("");
     }
-    
+    for (int i = 0; i < array.length; i++){
+        for (int j = 0; j < array.length; j++){
+            if (array[i][j] < 0){
+                array[i][j] = 0;
+            
+        }else if (array[i][j] > 0) { 
+                array[i][j] = 1;        
+            
+              
+        }
+    }
 }
+        
+        for (int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j] + "\t");
+        
+            }   
+        System.out.println("");
+        }
+        
+        }
+    }
+
